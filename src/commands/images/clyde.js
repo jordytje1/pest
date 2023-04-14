6dadc73ab68e184e0587290ec35c3a64557091e7
Almesaody <hameralmesaody@gmail.com>
2023-04-14 21:03:23 +0300

    Fixed some bugs

--- src/commands/images/clyde.js
+++ src/commands/images/clyde.js
@@ -1,10 +1,12 @@
+const { Canvas } = require("canvacord");
+const Discord = require("discord.js");
 module.exports = async (client, interaction, args) => {
 
     const clydeMessage = interaction.options.getString('text');
 
-    client.embed({
-        title: `🖼・Clyde`,
-        image: `https://ctk-api.herokuapp.com/clyde/${encodeURIComponent(clydeMessage)}`,
-        type: 'editreply'
-    }, interaction)
+    const image = await Canvas.clyde(clydeMessage)
+
+    const attachment = new Discord.AttachmentBuilder(image, "clyde.png");
+
+    interaction.editreply({ files: [attachment] });
 }
